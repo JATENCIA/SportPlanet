14be0a79fb775d64b4f7896316cd4777a1c21639
Santiago Massuh <smassuh@gmail.com>
2023-03-28 17:06:02 -0300

    productReview en proceso

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -4,10 +4,12 @@ const router = Router();
 const usersRoutes = require("./usersRoutes");
 // const storesRoutes = require("./storesRoutes");
 const productsRoutes = require("./productsRoutes");
+const productReviewRoutes = require("./productsReviewRoutes");
 
 router.use("/users", usersRoutes);
 // router.use("/stores", storesRoutes);
 router.use("/products", productsRoutes);
+router.use("/productReview", productReviewRoutes);
 
 // Importar todos los routers;
 //const UsersRoutes = require("./usersRoutes");
