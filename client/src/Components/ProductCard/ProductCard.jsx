73083b4f1c24412033855ac2d2c4292fa82d4856
sebas najle <sebastianrnajleok@gmail.com>
2023-03-28 15:24:37 -0300

    Card mod

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import styled from "./ProductCard.module.css"
 
-export default function ProductCard(){
+
+export default function ProductCard(props){
     return (
-        <div>
-            <h1>Soy una card</h1>
-            <h1>Soy una card</h1>
+        <div className={styled.h}>
+        <div className={styled.container}>
+        <NavLink to={`/detail/${props.id}`}>
+        <img src={props.img} alt="Not found" />
+        </NavLink>
+        <p>$299{props.precio}</p>
+        <p className={styled.p1}>Mismo precio en 3 cuotas de $99{props.cuotas}</p>
+        <p className={styled.p}>Envio gratis{props.envio}</p>
+    
         </div>
-
+        </div>
+    
+    
     )
+    
+
+    
 }
+
+
+import React from 'react'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
