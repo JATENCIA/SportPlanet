a627ceb7550a850e60513d85806b6fa4f80aed89
Santiago Massuh <smassuh@gmail.com>
2023-03-29 17:41:20 -0300

    cambio en modelo users

--- server/src/Models/Users.js
+++ server/src/Models/Users.js
@@ -74,7 +74,7 @@ const userSchema = mongoose.Schema(
     ],
     review: [
       {
-        type: Schema.Types.ObjectId,
+        type: mongoose.Types.ObjectId,
         ref: "ProductReview",
       },
     ],
