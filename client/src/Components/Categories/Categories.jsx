49544e0b8a58a59cd01e269f7892be2a51f59522
Alén <alenm.1717@gmail.com>
2023-03-29 18:54:35 -0300

    add categories

--- client/src/Components/Categories/Categories.jsx
+++ client/src/Components/Categories/Categories.jsx
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 
 export default function Categories(){
     return (
-     <div>
-<Link classname={style.Cardtshirts} to= "/category/t-shirts"><div>T-Shirts</div></Link>
-            <Link to='/category/pants'><div>Pants</div></Link>
-            <Link to='/category/shoes'><div>Shoes</div></Link>
-            <Link to='/category/balls'><div>Balls</div></Link>
-            <Link to='/category/supplements'><div>Supplements</div></Link>
-            <Link to='/category/accessories'><div>Accessories</div></Link>   
+     <div class={style.cointainer}>
+<Link gitto= "/category/t-shirts"><div className={style.TShirtsContainer}>T-Shirts</div></Link>
+            <Link to='/category/pants'><div className={style.PantsContainer}>Pants</div></Link>
+            <Link to='/category/shoes'><div className={style.ShoesContainer}>Shoes</div></Link>
+            <Link to='/category/balls'><div className={style.BallsContainer}>Balls</div></Link>
+            <Link to='/category/supplements'><div className={style.SupplementsContainer}>Supplements</div></Link>
+            <Link to='/category/accessories'><div className={style.AccessoriesContainer}>Accessories</div></Link>   
 </div>
-       
+
+      
     )
 }
