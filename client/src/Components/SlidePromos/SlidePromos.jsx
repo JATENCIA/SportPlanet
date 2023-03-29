3ac5afef3ec44b85a9e4cb8fd3365ebb66c88a72
sebas najle <sebastianrnajleok@gmail.com>
2023-03-29 14:01:45 -0300

    cambio imagen slide

--- client/src/Components/SlidePromos/SlidePromos.jsx
+++ client/src/Components/SlidePromos/SlidePromos.jsx
@@ -5,7 +5,7 @@ function SlidePromos() {
   return (
     <div className={styled.slider}>
         <ul >
-            <li><img src="https://assets.cybermonday.com.ar/uploads/offers/266322/6357e753d4d79.jpg?w=500&h=375"alt="" /></li> 
+            <li><img src="https://http2.mlstatic.com/storage/splinter-admin/o:f_jpg,q_auto:best/1678969548750-home-sliderdesktop.jpg"alt="" /></li> 
             <li><img src="https://www.belgranostudio.com/wp-content/uploads/2022/05/Hot-Sale-PNG.png" alt="" /></li>
             <li><img src="https://assets.hotsale.com.ar/uploads/offers/232159/628e75d76d8c2.jpg?w=500&h=375" alt="" /></li>
             <li><img src="https://ss-static-01.esmsv.com/id/145657/galeriaimagenes/obtenerimagen/?id=44&tipoEscala=stretch&width=1082&height=570" alt="" /></li>
