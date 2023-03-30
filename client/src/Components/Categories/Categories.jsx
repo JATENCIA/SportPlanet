8e913e0ec4f269bba789d7735178c1a5299c63be
sebas najle <sebastianrnajleok@gmail.com>
2023-03-30 13:48:01 -0300

    creando Tshirts

--- client/src/Components/Categories/Categories.jsx
+++ client/src/Components/Categories/Categories.jsx
@@ -9,7 +9,7 @@ export default function Categories(){
         <h2 className={style.Name}>Popular Categories</h2>
         </div>
      <div className={style.cointainer}>
-<Link className={style.TShirtsContainer} to= "/category/t-shirts"><div className={style.names}>T-Shirts</div></Link>
+<Link className={style.TShirtsContainer} to= "/category/tShirts"><div className={style.names}>T-Shirts</div></Link>
             <Link className={style.PantsContainer} to='/category/pants'><div className={style.names} >Pants</div></Link>
             <Link className={style.ShoesContainer} to='/category/shoes'><div className={style.names}>Shoes</div></Link>
             <div className={style.cointainer}> 
