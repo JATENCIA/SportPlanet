e9c7686080f2360e8821ec3ecf10d22dacdba7e6
Alen <alenm.1717@gmail.com>
2023-03-30 12:36:51 -0300

    Revert "Categories alen"

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Produts } from "../../Components/Produts";
 import SlidePromos from "../../Components/SlidePromos/SlidePromos";
-import  Categories  from "../../Components/Categories/Categories"
 import styled from "./Home.module.css"
 
 export const Home = () => {
   return (
     <div className={styled.main}>
       <SlidePromos/>
-      <Categories />
       <Produts />
       
     </div>
