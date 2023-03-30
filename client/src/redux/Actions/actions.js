e612d5ba461a7216710cf9417d792f226e2554ec
rake3344 <juan.agudelo17@udea.edu.co>
2023-03-30 11:27:54 -0500

    detail redux deleted

--- client/src/redux/Actions/actions.js
+++ client/src/redux/Actions/actions.js
@@ -1,9 +1,14 @@
 import axios from "axios";
-<<<<<<< HEAD
-import { GET_ALL_PRODUCT, GET_ALL_USER, POST_USER, FILTER_BY_GENDER, FILTER_BY_PRICE, FILTER_BY_SEASON, FILTER_BY_USED, FILTER_BY_SIZE } from "./actionsTypes";
-=======
-import { GET_ALL_PRODUCT, GET_ALL_USER, POST_USER, GET_PRODUCT_DETAIL } from "./actionsTypes";
->>>>>>> c205d0a (detail page no acabada)
+import {
+  GET_ALL_PRODUCT,
+  GET_ALL_USER,
+  POST_USER,
+  FILTER_BY_GENDER,
+  FILTER_BY_PRICE,
+  FILTER_BY_SEASON,
+  FILTER_BY_USED,
+  FILTER_BY_SIZE,
+} from "./actionsTypes";
 
 export const getAllUser = () => async (dispatch) => {
   try {
@@ -44,45 +49,42 @@ export const getAllProduct = () => async (dispatch) => {
   }
 };
 
-<<<<<<< HEAD
 export const filterByPrice = (payload) => {
   return {
     type: FILTER_BY_PRICE,
-    payload: payload
-  }
-}
+    payload: payload,
+  };
+};
 
 export const filterBySize = (payload) => {
   return {
     type: FILTER_BY_SIZE,
-    payload: payload
-  }
-}
+    payload: payload,
+  };
+};
 
 export const filterByUsed = (payload) => {
   return {
     type: FILTER_BY_USED,
-    payload: payload
-  }
-}
+    payload: payload,
+  };
+};
 export const filterByGender = (payload) => {
   return {
     type: FILTER_BY_GENDER,
-    payload: payload
-  }
-}
-=======
+    payload: payload,
+  };
+};
 
 // Detail of a product
 export const getProductDetail = (id) => async (dispatch) => {
   try {
-    const { data } = await axios.get(`/products/${id}`)
+    const { data } = await axios.get(`/products/${id}`);
     dispatch({
       type: GET_PRODUCT_DETAIL,
-      payload: data
-    })
+      payload: data,
+    });
   } catch (error) {
     return { messaje: `${error}` };
   }
-}
->>>>>>> c205d0a (detail page no acabada)
+};
--- client/src/redux/Reducer/reducer.js
+++ client/src/redux/Reducer/reducer.js
@@ -1,8 +1,13 @@
-<<<<<<< HEAD
-import { FILTER_BY_GENDER, FILTER_BY_PRICE, FILTER_BY_SIZE, FILTER_BY_USED, GET_ALL_PRODUCT, GET_ALL_USER, POST_USER } from "../Actions";
-=======
-import { GET_ALL_PRODUCT, GET_ALL_USER, POST_USER, GET_PRODUCT_DETAIL } from "../Actions";
->>>>>>> c205d0a (detail page no acabada)
+import {
+  FILTER_BY_GENDER,
+  FILTER_BY_PRICE,
+  FILTER_BY_SIZE,
+  FILTER_BY_USED,
+  GET_ALL_PRODUCT,
+  GET_ALL_USER,
+  POST_USER,
+  GET_PRODUCT_DETAIL,
+} from "../Actions";
 
 const initialState = {
   users: [],
@@ -29,61 +34,65 @@ export const rootReducer = (state = initialState, action) => {
         ...state,
         allProducts: action.payload,
       };
-      
-    case FILTER_BY_PRICE:
-      let productsSorted = action.payload === "min_weight" ? state.allProducts.sort((a, b) => {
-        if (a.price > b.price) return 1;
-        if (b.price> a.price) return -1;
-        return 0;
-      })
-    : state.allProducts.sort((a, b) => {
-        if (a.price > b.price) return -1;
-        if (b.price > a.price) return 1;
-        return 0;
-      });
 
-<<<<<<< HEAD
+    case FILTER_BY_PRICE:
+      let productsSorted =
+        action.payload === "min_weight"
+          ? state.allProducts.sort((a, b) => {
+              if (a.price > b.price) return 1;
+              if (b.price > a.price) return -1;
+              return 0;
+            })
+          : state.allProducts.sort((a, b) => {
+              if (a.price > b.price) return -1;
+              if (b.price > a.price) return 1;
+              return 0;
+            });
+
       return {
         ...state,
-        allProducts: productsSorted
-      }
+        allProducts: productsSorted,
+      };
 
     case FILTER_BY_USED:
-      let productsFiltered = action.payload === "new" ? state.allProducts.filter(product => {
-        return product.state === "new"
-      })
-      : state.allProducts.filter(product => product.state === "used")
+      let productsFiltered =
+        action.payload === "new"
+          ? state.allProducts.filter((product) => {
+              return product.state === "new";
+            })
+          : state.allProducts.filter((product) => product.state === "used");
       return {
         ...state,
-        allProducts: productsFiltered
-      }
-      
+        allProducts: productsFiltered,
+      };
+
     case FILTER_BY_GENDER:
-      let productsByGender = []
-      if(action.payload === "men"){
-        productsByGender = state.allProducts.filter(product => product.gender === "men")
-      }
-      else if(action.payload === "women"){
-        productsByGender = state.allProducts.filter(product => product.gender === "women")
-      }
-      else if(action.payload === "unisex"){
-        productsByGender = state.allProducts.filter(product => product.gender === "unisex")
-      }
-      else {
-        productsByGender = allProducts
+      let productsByGender = [];
+      if (action.payload === "men") {
+        productsByGender = state.allProducts.filter(
+          (product) => product.gender === "men"
+        );
+      } else if (action.payload === "women") {
+        productsByGender = state.allProducts.filter(
+          (product) => product.gender === "women"
+        );
+      } else if (action.payload === "unisex") {
+        productsByGender = state.allProducts.filter(
+          (product) => product.gender === "unisex"
+        );
+      } else {
+        productsByGender = allProducts;
       }
       return {
         ...state,
-        allProducts: [...productsByGender]
-      }  
-=======
+        allProducts: [...productsByGender],
+      };
     case GET_PRODUCT_DETAIL:
       return {
         ...state,
         productDetail: action.payload,
-      }
+      };
 
->>>>>>> c205d0a (detail page no acabada)
     default:
       return state;
   }
