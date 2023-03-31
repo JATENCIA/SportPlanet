3470174225d0e4f7ba32530e791d5b7a4d40cff2
Santiago Massuh <smassuh@gmail.com>
2023-03-31 11:29:01 -0300

    agregado de ruta

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -5,10 +5,12 @@ const usersRoutes = require("./usersRoutes");
 const storesRoutes = require("./storesRoutes");
 const productsRoutes = require("./productsRoutes");
 const productReviewRoutes = require("./productReviewRoutes");
+const userReviewRoutes = require("./userReviewRoutes");
 
 router.use("/users", usersRoutes);
 router.use("/stores", storesRoutes);
 router.use("/products", productsRoutes);
 router.use("/productReview", productReviewRoutes);
+router.use("/userReview", userReviewRoutes);
 
 module.exports = router;
