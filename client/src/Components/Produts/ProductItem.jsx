7d848f5d7fabc4869d26e2406a545684b21f8e0a
JATENCIA <atenciajaramilloj@gmail.com>
2023-04-01 10:45:25 -0500

    baneado de users

--- client/src/Components/Produts/ProductItem.jsx
+++ client/src/Components/Produts/ProductItem.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import styled from "./ProductItem.module.css"
-
-
+import styled from "./ProductItem.module.css";
 
 export const ProductItem = ({
   name,
@@ -12,29 +10,25 @@ export const ProductItem = ({
   size,
   image,
 }) => {
-  console.log("🚀 ~ file: ProductItem.jsx:12 ~ size:", size);
-
   return (
     <div className={styled.maincontainer}>
-    <div className={styled.container}>
-    <div className="flex">
- <img className="w-70 h-60" src={image[1]} alt="" />
- </div>
-{/*  <div className="bg-black w-30 h-4"></div> */}
-<div className={styled.p1}>
-      <div>
-        <h1 className="text-3xl text-lg">{name}</h1>
-      </div>
-      <div className="text-blue-500 text-xs">{description}</div>
-      
-      <div>{gender}</div>
-      <div>{state}</div>
-      
-      
-      
-       <div>$ {price}</div>
-       <p className={styled.p}>Envio Gratis!!</p>
-      </div>
+      <div className={styled.container}>
+        <div className="flex">
+          <img className="w-70 h-60" src={image[1]} alt="" />
+        </div>
+        {/*  <div className="bg-black w-30 h-4"></div> */}
+        <div className={styled.p1}>
+          <div>
+            <h1 className="text-3xl text-lg">{name}</h1>
+          </div>
+          <div className="text-blue-500 text-xs">{description}</div>
+
+          <div>{gender}</div>
+          <div>{state}</div>
+
+          <div>$ {price}</div>
+          <p className={styled.p}>Envio Gratis!!</p>
+        </div>
       </div>
     </div>
   );
