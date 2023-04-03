e2b7afa290f34913d340bd05df5d3d9c55948e19
Santiago SdeB <santiagosanchezdeb@gmail.com>
2023-04-03 11:37:10 -0300

    .

--- client/src/views/Forms/SellProduct/SellProduct.jsx
+++ client/src/views/Forms/SellProduct/SellProduct.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { NavBar } from "../../../Components/Navbar/Navbar";
+import FilterNavBar from '../../../Components/FilterNavBar/FilterNavBar'
 import style from './sellProduct.module.css'
 
 
@@ -118,18 +120,25 @@ export default function SellProduct(){
         
     };
 
+    const sellValidator = (values) => {
+        
+    }
+
     return (
+        <>
+        <NavBar />
+        <FilterNavBar />
         <div className={style.formContainer}>
         <form onSubmit={submitHandler}>
-            <h1 className="text-3xl font-bold text-gray-800 mb-5 text-center border-b-2 border-gray-800">Post the product you want to sell!</h1>
+            <h1 className="text-3xl font-bold text-gray-900 mb-5 text-center border-b-2 border-gray-800">Post the product you want to sell!</h1>
             {console.log(post)}
             <div>
-            <label className="block text-gray-700 text-xl font-bold">Product Name</label>
+            <label className="block text-gray-800 text-xl font-bold">Product Name</label>
             <input type="text" name='name' value={post.name} onChange={changeHandler} className={style.input} />
             </div>
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Image</label>
+            <label className="block text-gray-800 text-xl font-bold">Image</label>
             <input type='text' name='image' onChange={imageChangeHandler} className={style.input}/>
             </div>
 
@@ -152,7 +161,7 @@ export default function SellProduct(){
             }
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Set a Price</label>
+            <label className="block text-gray-800 text-xl font-bold">Set a Price</label>
             <div>
             <select className={style.sizeSelect}>
                 <option> </option>
@@ -163,12 +172,12 @@ export default function SellProduct(){
             </div>
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Description</label>
+            <label className="block text-gray-800 text-xl font-bold">Description</label>
             <textarea type='text' name='description' value={post.description} onChange={changeHandler} className={style.input}/>
             </div>
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Season</label>
+            <label className="block text-gray-800 text-xl font-bold">Season</label>
             <select onChange={changeHandler} name='season' value={post.season} className={style.input}>
                 <option> </option>
                 {yearOptions}
@@ -176,7 +185,7 @@ export default function SellProduct(){
             </div>
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Category</label>
+            <label className="block text-gray-800 text-xl font-bold">Category</label>
             <select onChange={categoryChangeHandler} name='category' value={post.category} className={style.input}>
                 <option disabled> </option>
                 <option value='tshirts'>T-Shirts</option>
@@ -193,7 +202,7 @@ export default function SellProduct(){
             {post.category === "tshirts" || post.category === "pants"
             ?
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Size</label>
+            <label className="block text-gray-800 text-xl font-bold">Size</label>
 
             <div>
             <select defaultValue={sizeS} className={style.sizeSelect}>
@@ -233,7 +242,7 @@ export default function SellProduct(){
             </div>
             : 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Amount</label>
+            <label className="block text-gray-800 text-xl font-bold">Amount</label>
             <input type='number' className={style.input} value={amount !== 0 ? amount : ""} onChange={amountHandler}/>
             </div>
             }
@@ -242,7 +251,7 @@ export default function SellProduct(){
 
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Gender</label>
+            <label className="block text-gray-800 text-xl font-bold">Gender</label>
             <select onChange={changeHandler} name='gender' value={post.gender} className={style.input}>
                 <option> </option>
                 <option value='unisex'>Unisex</option>
@@ -252,7 +261,7 @@ export default function SellProduct(){
             </div>
 
             <div className="mt-5">
-            <label className="block text-gray-700 text-xl font-bold">Condition</label>
+            <label className="block text-gray-800 text-xl font-bold">Condition</label>
             <select onChange={changeHandler} name='state' value={post.state} className={style.input}> 
                 <option> </option>
                 <option value='new'>New</option>
@@ -261,7 +270,7 @@ export default function SellProduct(){
             </div>
 
             <div className="mt-5">
-                <label className="block text-gray-700 text-xl font-bold">Brand</label>
+                <label className="block text-gray-800 text-xl font-bold">Brand</label>
                 <select onChange={changeHandler} name='brand' value={post.brand} className={style.input}>
                     <option> </option>
                     <option value='ADIDAS'>Adidas</option>
@@ -273,8 +282,9 @@ export default function SellProduct(){
                 </select>
             </div>
 
-            <div className="mt-8"><button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto block">POST THIS PRODUCT</button></div>
+            <div className="mt-8"><button type="submit" className="bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded mx-auto block">POST THIS PRODUCT</button></div>
         </form>
         </div>
+        </>
     )
 }
