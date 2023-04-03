ff6c29d1619bbb3743c34ad11cbc9dd751de031c
ADMIN <panchobaca10@gmail.com>
2023-04-03 14:18:32 -0300

    Predemo 1.1

--- client/src/views/SearchedProducts/SearchedProducts.jsx
+++ client/src/views/SearchedProducts/SearchedProducts.jsx
@@ -1,43 +1,42 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { getSearchedProducts } from "../../redux/Actions/actions";
-import { ProductItem } from "../../Components/Produts/ProductItem"
+import { ProductItem } from "../../Components/Produts/ProductItem";
 import { NavBar } from "../../Components/Navbar";
-import  FilterNavBar from "../../Components/FilterNavBar/FilterNavBar";
+import FilterNavBar from "../../Components/FilterNavBar/FilterNavBar";
 import { Link } from "react-router-dom";
 
 const SearchedProducts = () => {
-    const dispatch = useDispatch();
-    const searchedProducts = useSelector(state=>state.searchedProducts)
-    const { product } = useParams();
-    
-    useEffect(()=>{
-        dispatch(getSearchedProducts(product))
+  const dispatch = useDispatch();
+  const searchedProducts = useSelector((state) => state.searchedProducts);
+  const { product } = useParams();
 
-        //return para limpiar searchedProducts
-    },[dispatch, product])
+  useEffect(() => {
+    dispatch(getSearchedProducts(product));
+    //return para limpiar searchedProducts
+  }, [dispatch, product]);
 
-    return(
-        <div>
-            <NavBar />
-<FilterNavBar />
-            {searchedProducts.map((product) => (
-                <Link to={`/detail/${product._id}`}>
-                <ProductItem
-                    key={product.id} 
-                    name={product.name}
-                    description={product.description}
-                    price={product.price}
-                    gender={product.gender}
-                    state={product.state}
-                    size={product.size}
-                    image={product.image}
-                />
-               </Link>
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavBar />
+      <FilterNavBar />
+      {searchedProducts.map((product) => (
+        <Link to={`/detail/${product._id}`}>
+          <ProductItem
+            key={product.id}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+            gender={product.gender}
+            state={product.state}
+            size={product.size}
+            image={product.image}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default SearchedProducts;
