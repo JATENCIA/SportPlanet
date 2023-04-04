cda7d0094f98ebf104971ff001f1c67ae8b2010e
Santiago Massuh <smassuh@gmail.com>
2023-04-04 16:58:04 -0300

    cambios

--- server/src/Routes/index.js
+++ server/src/Routes/index.js
@@ -7,27 +7,17 @@ const router = Router();
 // Configurar los routers
 //router.use("/users", UsersRoutes);
 
-
 const usersRoutes = require("./usersRoutes");
 // const storesRoutes = require("./storesRoutes");
 const productsRoutes = require("./productsRoutes");
 const productReviewRoutes = require("./productReviewRoutes");
-<<<<<<< HEAD
 // const userReviewRoutes = require("./userReviewRoutes");
-=======
-const paymentsRoutes = require("./paymentsRoutes");
->>>>>>> 436e1ce4c5765e77e2cd36c0978276feb2fadb22
 
 router.use("/users", usersRoutes);
 // router.use("/stores", storesRoutes);
 router.use("/products", productsRoutes);
 router.use("/productReview", productReviewRoutes);
-<<<<<<< HEAD
 // router.use("/userReview", userReviewRoutes);
-=======
-router.use("/payments", paymentsRoutes);
-
->>>>>>> 436e1ce4c5765e77e2cd36c0978276feb2fadb22
 
 // Importar todos los routers;
 const UsersRoutes = require("./usersRoutes");
