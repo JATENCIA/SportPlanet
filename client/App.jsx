7af815a0aefb649de7b28cbf1060cd8da57d2a39
Alén <alenm.1717@gmail.com>
2023-04-11 14:12:30 -0300

    cambios en FilterNavBar

--- client/App.jsx
+++ client/App.jsx
@@ -36,12 +36,11 @@ import ProfileShopping from "./src/views/Profile/ProfileShopping/ProfileShopping
 import ProfileSales from "./src/views/Profile/ProfileSales/ProfileSales";
 import ProfileFavorites from "./src/views/Profile/ProfileFavorites/ProfileFavorites";
 import ProfileProducts from "./src/views/Profile/ProfileProducts/ProfileProducts";
-
 import Man from "./src/Components/Categories/Man/Man";
 import Woman from "./src/Components/Categories/Woman/Woman";
 import Kids from "./src/Components/Categories/Kids/Kids";
 import Promotions from "./src/Components/Categories/Promotions/Promotions";
-import Fitness from "./src/Components/Categories/Fitness/Fitness";
+
 
 import ProfileReviews from "./src/views/Profile/ProfileReviews/ProfileReviews";
 import AdminShopping from "./src/views/Admin/AdminShopping/AdminShopping";
@@ -99,7 +98,7 @@ const App = () => {
         <Route path="/category/woman" Component={Woman} />
         <Route path="/category/kids" Component={Kids} />
         <Route path="/category/promotions" Component={Promotions} />
-        <Route path="/category/fitness" Component={Fitness} />
+        
 
       </Routes>
     </>
