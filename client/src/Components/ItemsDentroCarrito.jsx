b1e5f715332231f7218a8130242ad44f69be7ff8
JATENCIA <atenciajaramilloj@gmail.com>
2023-04-17 10:39:25 -0500

    bug solucionado

--- client/src/Components/ItemsDentroCarrito.jsx
+++ client/src/Components/ItemsDentroCarrito.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "./ItemsDentroCarrito.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function ItemsDentroCarrito() {
   const cart = useSelector((state) => state.shoppingCart);
-
+  const { isAuthenticated, user, logout } = useAuth0();
   const itemsCarrito = cart.reduce((acc, el) => acc + el.quantity, 0);
 
   return (
     <div className={styled.border}>
       <span className={styled.items}>
-        {itemsCarrito !== 0 ? itemsCarrito : null}
+        {itemsCarrito !== 0
+          ? user && isAuthenticated
+            ? itemsCarrito
+            : null
+          : null}
       </span>
     </div>
   );
