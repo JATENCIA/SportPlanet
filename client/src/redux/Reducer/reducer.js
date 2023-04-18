5ae4c817c2bb448cb2a6ab17a69878bb9d029c62
JATENCIA <atenciajaramilloj@gmail.com>
2023-04-17 23:29:24 -0500

    mercado pago al 99.9%, solucionado los filtros por size

--- client/src/redux/Reducer/reducer.js
+++ client/src/redux/Reducer/reducer.js
@@ -123,50 +123,53 @@ export const rootReducer = (state = initialState, action) => {
         ...state,
         productDetail: action.payload,
       };
-    case FILTER_BY_SIZE:
-    /*   let productBySize = [...state.allProducts] */
-    /*     .sort((a, b) => { */
-    /*       const sizeValues = { small: 1, medium: 2, large: 2, xlarge: 4 }; */
-    /*       const aSizeValues = sizeValues[a.size]; */
-    /*       const bSizeValues = sizeValues[b.size]; */
-    /*       return aSizeValues - bSizeValues; */
-    /*     }) */
-    /*     .filter((product) => product.size === action.payload); */
-    /*   return { */
-    /*     ...state, */
-    /*     filteredProducts: productBySize, */
-    /*   }; */
 
     case FILTER_BY_SIZE:
       let productSize = [];
-      if (action.payload === "small") {
-        productSize = [...state.filteredProducts2].filter(
-          (e) => e.productConditionals[0].size[0].S > 0
+      let filtro2 = [...state.filteredProducts2];
+      if (action.payload === "xSmall") {
+        productSize = filtro2.filter((e) =>
+          e.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("XS"))
+          )
+        );
+      } else if (action.payload === "small") {
+        productSize = filtro2.filter((e) =>
+          e.productConditionals.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("S"))
+          )
         );
       } else if (action.payload === "medium") {
-        productSize = [...state.filteredProducts2].filter(
-          (e) => e.productConditionals[0].size[0].M > 0
+        productSize = filtro2.filter((e) =>
+          e.productConditionals.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("M"))
+          )
         );
       } else if (action.payload === "large") {
-        productSize = [...state.filteredProducts2].filter(
-          (e) => e.productConditionals[0].size[0].L > 0
+        productSize = filtro2.filter((e) =>
+          e.productConditionals.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("L"))
+          )
         );
       } else if (action.payload === "xlarge") {
-        productSize = [...state.filteredProducts2].filter(
-          (e) => e.productConditionals[0].size[1].XL > 0
+        productSize = filtro2.filter((e) =>
+          e.productConditionals.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("XL"))
+          )
+        );
+      } else if (action.payload === "xxlarge") {
+        productSize = filtro2.filter((e) =>
+          e.productConditionals.some((elem) =>
+            elem.size.some((elem2) => elem2.hasOwnProperty("XXL"))
+          )
         );
       } else {
-        /* else if (action.payload === "XL"){ */
-        /*   productSize = [...state.allProducts].filter(e => e.size[0].XL >= 0 ) */
-        /*    */
-        /* } */
         productSize = state.allProducts;
       }
 
       return {
         ...state,
         filteredProducts: [...productSize],
-        filteredProducts2: [...productSize],
       };
 
     case FILTER_BY_SEASON:
