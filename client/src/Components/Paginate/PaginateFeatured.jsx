2e88ebc6c159a4e76f8d4a2d421854cab7c9f12c
JATENCIA <atenciajaramilloj@gmail.com>
2023-04-19 09:40:56 -0500

    products featured al 99.9%

---
+++ client/src/Components/Paginate/PaginateFeatured.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import style from "./Paginate.module.css";
+
+export function PaginateFeatured({
+  allProducts,
+  productsPerPage,
+  setPagination,
+  currentPage,
+  setCurrentPage,
+}) {
+  const pageNumber = [];
+
+  for (let i = 1; i <= Math.ceil(allProducts / productsPerPage); i++) {
+    pageNumber.push(i);
+  }
+
+  const handlePrev = () => {
+    if (currentPage === 1) setCurrentPage(1);
+    else setCurrentPage(currentPage - 1);
+  };
+
+  const handleNext = () => {
+    //next
+    if (currentPage === pageNumber[pageNumber.length - 1])
+      setCurrentPage(currentPage);
+    else setCurrentPage(currentPage + 1);
+  };
+
+  return (
+    <div className={style.paginate_container}>
+      <div className={style.prev_next}>
+        <button
+          className={style.next_prev_btn}
+          onClick={() => {
+            handlePrev();
+          }}
+          disabled={currentPage === 1}
+        >
+          {" "}
+          <i className="fa-solid fa-angle-left" />{" "}
+        </button>
+      </div>
+
+      <div className={style.pages}>
+        {pageNumber &&
+          pageNumber.map((n) => {
+            return n !== currentPage ? (
+              <div className={style.page} key={n}>
+                <button
+                  className={style.numPage}
+                  key={n}
+                  onClick={() => {
+                    scroll();
+                    setPagination(n);
+                  }}
+                >
+                  {n}
+                </button>
+              </div>
+            ) : (
+              <div className={style.page} key={n}>
+                <button
+                  className={style.numPageSelected}
+                  key={n}
+                  onClick={() => {
+                    scroll();
+                    setPagination(n);
+                  }}
+                >
+                  {n}
+                </button>
+              </div>
+            );
+          })}
+      </div>
+      <div className={style.prev_next}>
+        <button
+          className={style.next_prev_btn}
+          onClick={() => {
+            scroll();
+            handleNext();
+          }}
+          disabled={currentPage === pageNumber.length}
+        >
+          {" "}
+          <i className="fa-solid fa-angle-right" />{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
