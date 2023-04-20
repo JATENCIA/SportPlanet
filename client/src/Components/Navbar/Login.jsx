c2162bac0f70c497d43ed43d91f4b5721cdf1561
Santiago Massuh <smassuh@gmail.com>
2023-04-20 01:09:18 -0300

    actualizacion

--- client/src/Components/Navbar/Login.jsx
+++ client/src/Components/Navbar/Login.jsx
@@ -96,8 +96,8 @@ const Login = () => {
           </MenuItem>
           <MenuItem className="p-0 hover:bg-transparent">
             <Link
-              to="#"
-              onClick={() => logout({ returnTo: window.location.origin })}
+              to="/home" //#
+              // onClick={() => logout({ returnTo: window.location.origin })}
               className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
             >
               <RiLogoutCircleRLine /> Log Out
@@ -185,8 +185,8 @@ const Login = () => {
               </MenuItem>
               <MenuItem className="p-0 hover:bg-transparent">
                 <Link
-                  to="#"
-                  onClick={() => logout({ returnTo: window.location.origin })}
+                  to="/home" //#
+                  // onClick={() => logout({ returnTo: window.location.origin })}
                   className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
                 >
                   <RiLogoutCircleRLine /> Log Out
