795f8fdb4626bcd774f065247a8a2183f390fb69
JATENCIA <atenciajaramilloj@gmail.com>
2023-04-20 13:04:07 -0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,17 +10,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
-axios.defaults.baseURL = "http://localhost:5000";
-// axios.defaults.baseURL = "https://sportsplanet-production.up.railway.app/";
+// axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://sportsplanet-production.up.railway.app/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <Auth0Provider
-        // domain="dev-uifyeswryeh53bj1.us.auth0.com"
-        // clientId="3A17pliIGOkuzXgriNiVChRPGJnHQ4Eh"
-        domain="dev-j0kao75n5a8hszfj.us.auth0.com"
-        clientId="okBJLqsiuORgexDsYLGBogOaVw0mT4Gc"
+        domain="dev-uifyeswryeh53bj1.us.auth0.com"
+        clientId="3A17pliIGOkuzXgriNiVChRPGJnHQ4Eh"
+        // domain="dev-j0kao75n5a8hszfj.us.auth0.com"
+        // clientId="okBJLqsiuORgexDsYLGBogOaVw0mT4Gc"
         redirectUri={window.location.origin}
       >
         <App />
